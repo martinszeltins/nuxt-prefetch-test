3f66940bcd73a4c55579aa4b567acc61812fbc39
Martins Zeltins <martins.zeltins@arkbauer.com>
2024-11-08 09:45:50 +0200

    [master] feat: add prefetching

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt']
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  app: {
+    head: {
+        link: [
+            { rel: 'prefetch', href: '/users' }
+        ]
+    }
+  }
 })
